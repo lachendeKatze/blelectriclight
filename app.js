f0e5010b9c359b79cbe57822bbedd812e0f2eec7
lachendeKatze <govkn@yahoo.com>
2016-11-05 14:01:26 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+/**
+* app.js is the file connecting elements and actions
+* 11/5/2016
+*/
+
+
+/**
+ * Toggles the opacity of the selected circle representing an LED, then
+ * passes off to toggleLight() to change the LED state on the actual
+ * arduino101
+ */
+function lightSwitch()
+{
+   blelectriclight._writeCharacteristic(blelectriclight.switchUUID, new Uint8Array([4]))
+}
+/**
+function lightIt(circleID)
+{
+  var circle = document.getElementById(circleID);
+  var currentOpacity = circle.getAttribute("fill-opacity");
+  console.log("current 0 opacity = " + currentOpacity);
+  var curOp = parseFloat(currentOpacity);
+  console.log("curOp: " + curOp);
+ if ( currentOpacity < 0.8 ) {
+  // currentOpacity = currentOpacity + 0.2;
+   curOp = curOp + 0.2;
+  circle.setAttribute("fill-opacity", curOp);
+ }
+ else {
+  circle.setAttribute("fill-opacity", 0.2);
+ }
+ toggleLight(circleID);
+};
+
+function getBrightness()
+{
+    lightReading = exp3._readCharacteristic(exp3.cds);
+    console.log('light: ' + lightReading);
+}
+
+/**
+  * takes the chosen "circle" and writes the appropriate data
+  * to the ledservice characteristics.
+
+var toggleLight = function( circleID )
+{
+  if (circleID == "circle1" ) {
+   exp3._writeCharacteristic(exp3.ledUUID, new Uint8Array([4]))
+   .then(() => console.log('wrote 4'))
+   .catch(error => {console.log('write error');
+   });
+  }
+  else if (circleID == "circle2" ){
+    exp3._writeCharacteristic(exp3.ledUUID, new Uint8Array([3]))
+   .then(() => console.log('wrote 3'))
+   .catch(error => {console.log('write error');
+   });
+  }
+  else if (circleID == "circle3") {
+    exp3._writeCharacteristic(exp3.ledUUID, new Uint8Array([2]))
+   .then(() => console.log('wrote 2'))
+   .catch(error => {console.log('write error');
+   })
+  }
+  else{
+    exp3._writeCharacteristic(exp3.ledUUID, new Uint8Array([1]))
+   .then(() => console.log('wrote 1'))
+   .catch(error => {console.log('write error');
+   });
+  }
+};
+*/
+/**
+  * The Web Bluetooth API specifies that a user action like a click is needed to
+  * initiate a connection.
+  */
+function bleConnect(buttonID) {
+  var thisButton = document.getElementById(buttonID);
+  thisButton.setAttribute("fill-opacity",0.9);
+  blectriclight.connect()
+      .then(() => console.log('connected'))
+      .catch(error => { console.log('connect error!'); });
+
+};
---
+++ blelectricLight.js
@@ -0,0 +1,70 @@
+(function() {
+  'use strict';
+
+  class BLElectricLight{
+
+    /**
+      * The LED class is a object used to represent the BLE service. Note for this project
+      * the read code is not used, but I kept it there for you to see.
+      *
+      * customize your project here to reflect the uuid of your service and characteristics.
+      */
+    constructor() {
+        this.deviceName = 'lamp'; // BLElectricLight Device Name
+        this.serviceUUID =  '917649A0-D98E-11E5-9EEC-0002A5D5C51B'; // BLElectricLight service uuid
+        this.switchUUID = '917649A1-D98E-11E5-9EEC-0002A5D5C51B'; // On/Off switch uuid
+        this.brightnessUUID = '917649A2-D98E-11E5-9EEC-0002A5D5C51B'; // Brightness uuid
+        this.colorUUID = '917649A3-D98E-11E5-9EEC-0002A5D5C51B'; // color uuid, holds 3 values G(reen)R(ed)B(lue)
+        // this.patternUUID = '917649A4-D98E-11E5-9EEC-0002A5D5C51B'; // pattern uuid
+        this.device = null;
+        this.server = null;
+        // The cache allows us to hold on to characeristics for access in response to user commands
+        this._characteristics = new Map();
+    }
+
+    connect(){
+
+      return navigator.bluetooth.requestDevice({
+        filters:[{
+          services:[this.serviceUUID],
+        }]
+      }).then(device => device.gatt.connect())
+      .then(server => server.getPrimaryService(this.serviceUUID))
+      .then(service => {
+        return Promise.all([
+          this._cacheCharacteristic(service, this.colorUUID),
+          this._cacheCharacteristic(service, this.brightnessUUID)
+          this._cacheCharacteristic(service, this.switchUUID),
+        ]);
+      })
+    }
+
+  _cacheCharacteristic(service, characteristicUuid){
+    return service.getCharacteristic(characteristicUuid)
+    .then(characteristic => {
+      this._characteristics.set(characteristicUuid, characteristic).then(console.log('cache: '  + characteristicUuid));
+    });
+  }
+
+ _readCharacteristic(characteristicUuid) {
+   let characteristic = this._characteristics.get(characteristicUuid);
+   console.log("read this char: " + characteristicUuid);
+   console.log("this is thechar: " + characteristic);
+   return characteristic.readValue()
+   .then(value => {console.log('value: ' + value);
+     return value;
+   });
+
+ }
+ _writeCharacteristic(characteristicUuid, value){
+   let characteristic = this._characteristics.get(characteristicUuid);
+   console.log("the write char: " + characteristic);
+   return characteristic.writeValue(value);
+ }
+}
+
+
+// makes the led object available to therest of the program, remeber to change this to reflect your project
+window.blelectriclight = new BLElectricLight();
+
+})();
