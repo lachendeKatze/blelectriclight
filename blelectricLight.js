b135a3395eae478004148453a30d84c669c140e9
lachendeKatze <govkn@yahoo.com>
2016-11-05 19:18:10 -0400

    Update blelectricLight.js

--- blelectricLight.js
+++ blelectricLight.js
@@ -38,9 +38,9 @@
           server.getPrimaryService(this.serviceUUID)
           .then(service=>{  
             return Promise.all([
-              this._cacheCharacteristic(service, this.switchUUID),
+              this._cacheCharacteristic(service, this.colorUUID),
               this._cacheCharacteristic(service, this.brightnessUUID),
-              this._cacheCharacteristic(service, this.colorUUID)
+              this._cacheCharacteristic(service, this.switchUUID)
             ])
           })
         ]);
