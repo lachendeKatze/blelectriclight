1b521ddda997ed44a86c82497d77bcc95b0620a4
lachendeKatze <govkn@yahoo.com>
2016-11-05 22:13:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,24 +14,16 @@ function brightnessSelect()
             console.log( 'brightness: ' + x);
 }
 
-var canvas = document.getElementById("picker");
-var ctx = canvas.getContext("2d");
+// var canvas = document.getElementById("picker");
+// var ctx = canvas.getContext("2d");
 
-document.getElementById("scream").onload = function() {
-var c = document.getElementById("picker");
-var ctx = c.getContext("2d");
-var img = document.getElementById("scream");
-    ctx.drawImage(img, 0, 0);
-    var imgData = ctx.getImageData(0, 0, c.width, c.height);
-    // invert colors
-    var i;
-    for (i = 0; i < imgData.data.length; i += 4) {
-        imgData.data[i] = 255 - imgData.data[i];
-        imgData.data[i+1] = 255 - imgData.data[i+1];
-        imgData.data[i+2] = 255 - imgData.data[i+2];
-        imgData.data[i+3] = 255;
-    }
-    ctx.putImageData(imgData, 0, 0);
+document.getElementById("wheel").onload = function() 
+{
+   var c = document.getElementById("picker");
+   var ctx = c.getContext("2d");
+   var img = document.getElementById("wheel");
+   ctx.drawImage(img, 0, 0);
+   var imgData = ctx.getImageData(0, 0, c.width, c.height);
 };
 
 
