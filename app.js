0ae228e49d8b8b061c6f4583e801fc077c90b651
lachendeKatze <govkn@yahoo.com>
2016-11-05 22:33:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,30 @@ function brightnessSelect()
 var canvas = document.getElementById("picker");
 canvas.addEventListener("mousedown", getPosition, false);
 
+function getPosition(event)
+      {
+        var x = new Number();
+        var y = new Number();
+        var canvas = document.getElementById("canvas");
+
+        if (event.x != undefined && event.y != undefined)
+        {
+          x = event.x;
+          y = event.y;
+        }
+        else // Firefox method to get the position
+        {
+          x = event.clientX + document.body.scrollLeft +
+              document.documentElement.scrollLeft;
+          y = event.clientY + document.body.scrollTop +
+              document.documentElement.scrollTop;
+        }
+
+        x -= canvas.offsetLeft;
+        y -= canvas.offsetTop;
+
+        alert("x: " + x + "  y: " + y);
+}
 
 document.getElementById("wheel").onload = function() 
 {
@@ -26,6 +50,11 @@ document.getElementById("wheel").onload = function()
    ctx.drawImage(img, 0, 0);
    var imgData = ctx.getImageData(0, 0, c.width, c.height);
 };
+
+
+
+
+
 
 
 // var img = new Image();
