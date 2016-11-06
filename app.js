09811a5826da3ebe7289bc5e1afeb5f8ac492456
lachendeKatze <govkn@yahoo.com>
2016-11-05 23:18:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,19 +15,12 @@ function brightnessSelect()
 }
 
 var canvas = document.getElementById("picker");
-
-
-
-
-   // var c = document.getElementById("picker");
-   var ctx = canvas.getContext("2d");
-   var img = new Image();
-   img.src = "./images/color_wheel_730.png";
+var ctx = canvas.getContext("2d");
+var img = new Image();
+img.src = "./images/color_wheel_730.png";
    // var img = document.getElementById("wheel");
-   ctx.drawImage(img, 0, 0);
-   var imgData = ctx.getImageData(0, 0, c.width, c.height);
-
-
+canvas.drawImage(img, 0, 0);
+// var imgData = ctx.getImageData(0, 0, c.width, c.height);
 canvas.addEventListener("mousedown", getPosition, false);
 
 function getPosition(evt)
