b7f945a280d461521dd422b02f7a028f6afc9c21
lachendeKatze <govkn@yahoo.com>
2016-11-05 22:08:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,25 @@ function brightnessSelect()
 
 var canvas = document.getElementById("picker");
 var ctx = canvas.getContext("2d");
+
+document.getElementById("scream").onload = function() {
+var c = document.getElementById("picker");
+var ctx = c.getContext("2d");
+var img = document.getElementById("scream");
+    ctx.drawImage(img, 0, 0);
+    var imgData = ctx.getImageData(0, 0, c.width, c.height);
+    // invert colors
+    var i;
+    for (i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] = 255 - imgData.data[i];
+        imgData.data[i+1] = 255 - imgData.data[i+1];
+        imgData.data[i+2] = 255 - imgData.data[i+2];
+        imgData.data[i+3] = 255;
+    }
+    ctx.putImageData(imgData, 0, 0);
+};
+
+
 // var img = new Image();
 // img.src = 'blelectriclight/images/color_wheel_730.png';
         // img.src = 'lizard.png'
