6ea4db64140b7f7ef585e422e6f34d56975880b6
lachendeKatze <govkn@yahoo.com>
2016-12-28 11:35:23 -0500

    Add files via upload

--- BLElectricLight/BLElectricLight.ino
+++ BLElectricLight/BLElectricLight.ino
@@ -3,8 +3,8 @@
 
 /**
 * This project demonstrates how to control an adafruit industries NeoPixel Ring over BLE using an arduino/genuino101.
-* This projct is derived from the Cproject in Chapter 7 of Make: Bluetooth: Bluetooth LE Projects with Arduino, Raspberry Pi,
-* and Smartphones and the project by the Ruiz brothers from Adafruit entitled,(http://www.thingiverse.com/thing:1343925)
+* This projct is derived from the Cproject in Chapter 7 of Make: Bluetooth: Bluetooth LE Projects with Arduino, Raspberry Pi, 
+* and Smartphones and the project by the Ruiz brothers from Adafruit entitled,(http://www.thingiverse.com/thing:1343925) 
 */
 
 /**
@@ -12,20 +12,19 @@
 */
 BLEPeripheral blePeripheral;       // BLE Peripheral Device (the board you're programming)
 BLEService lampService("917649A0-D98E-11E5-9EEC-0002A5D5C51B"); // Custom UUID
-BLEUnsignedCharCharacteristic switchCharacteristic("917649A1-D98E-11E5-9EEC-0002A5D5C51B", BLEWrite);
-BLEUnsignedCharCharacteristic brightnessCharacteristic("917649A2-D98E-11E5-9EEC-0002A5D5C51B", BLEWrite);
-BLECharacteristic colorCharacteristic("917649A3-D98E-11E5-9EEC-0002A5D5C51B", BLEWrite,3);
-BLECharacteristic patternCharacteristic("917649A4-D98E-11E5-9EEC-0002A5D5C51B", BLEWrite);
-// BLEDescriptor lampDescriptor("2902", "lamp");
+BLEUnsignedCharCharacteristic switchCharacteristic("917649A1-D98E-11E5-9EEC-0002A5D5C51B", BLERead | BLEWrite);
+BLEUnsignedCharCharacteristic brightnessCharacteristic("917649A2-D98E-11E5-9EEC-0002A5D5C51B", BLERead | BLEWrite);
+BLECharacteristic colorCharacteristic("917649A3-D98E-11E5-9EEC-0002A5D5C51B", BLERead | BLEWrite,3);
+BLEDescriptor lampDescriptor("2902", "lamp");
 
 /**
  * establish which pin the NeoPixel ring is attachd to on the Arduino/Genuino101, and the
  * number of NeoPixels on the ring.
- *
+ * 
 */
 #define PIN            7
 #define NUMPIXELS      12
-/*
+/* 
  *  When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
  *  Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
  *  example for more information on possible values.
@@ -50,19 +49,19 @@ struct LampStateStructure lampState;
 
 void setup() {
 
-  /** Serial
+  /** Serial 
   * Serial communication is not necessary for this project to work, but
-  * is very useful for debugging. All serial references could be
+  * is very useful for debugging. All serial references could be 
   * commented out if desried.
   */
   Serial.begin(9600); // initialize Serial communication
   while (!Serial);    // wait for the serial port to open
-
+ 
   Serial.println("Arduino101/IntelCurie/NeoPixel-Voroni-Lamp/Evothings Example Started");
   Serial.println("Serial rate set to 9600");
 
   // initilize the ble service, characterisitcs and advertising
-  blePeripheral.setLocalName("lamp");
+  blePeripheral.setLocalName("voronoi");
   blePeripheral.setAdvertisedServiceUuid(lampService.uuid());
   blePeripheral.addAttribute(lampService);
   blePeripheral.addAttribute(switchCharacteristic);
@@ -80,22 +79,22 @@ void setup() {
   // set an initial values for the characteristics
   switchCharacteristic.setValue(-1);
   brightnessCharacteristic.setValue(0);
-
-  /**
+  
+  /** 
    * the color characterisitc consists of the 3 GREEN,RED and BLUE values sent
-   * from the mobile device as a single characterisitic of 3 bytes, each a
+   * from the mobile device as a single characterisitic of 3 bytes, each a 
    * value from 0..255
   */
-
+  
   const unsigned char initialColor[3] = {0,0,0};
   colorCharacteristic.setValue(initialColor,3);
-
+  
   // advertise the service
   blePeripheral.begin();
   Serial.println(("Bluetooth device active, waiting for connections..."));
 
   // setup the lamp state variable strucutre and set to intial values
-
+  
   lampState.lampOn = false;
   lampState.brightness = 200;
   lampState.green = 250;
@@ -104,7 +103,7 @@ void setup() {
 
 
   // This initializes the NeoPixel, read the code in the NeoPixellibrary to understand what it does.
-  ring.begin();
+  ring.begin(); 
 }
 
 void loop() {
@@ -112,21 +111,21 @@ void loop() {
 
   // continual poll the ble device for activity, connection, disconnect, characterisitc cahgnes, etc.
   blePeripheral.poll();
-
+  
 } // end loop
 
 /**
  * BLE Event Handlers are defined in the fucntions below
  */
-
-void blePeripheralConnectHandler(BLECentral& central)
+ 
+void blePeripheralConnectHandler(BLECentral& central) 
 {
   // central connected event handler
   Serial.print("Connected event, central: ");
   Serial.println(central.address());
 }
 
-void blePeripheralDisconnectHandler(BLECentral& central)
+void blePeripheralDisconnectHandler(BLECentral& central) 
 {
   // central disconnected event handler
   Serial.print("Disconnected event, central: ");
@@ -136,7 +135,7 @@ void blePeripheralDisconnectHandler(BLECentral& central)
 void switchCharacteristicWritten(BLECentral& central, BLECharacteristic& characteristic)
 {
   /**
-   * We will use a switch/case statement here so that we can expand the functionality of the app
+   * We will use a switch/case statement here so that we can expand the functionality of the app 
    * in the future; This will allow us to accept values up to 255 on the switch characteristic
    * and not permanently tie the characteritis to just turning the lamp on/off;
    */
@@ -153,13 +152,13 @@ void switchCharacteristicWritten(BLECentral& central, BLECharacteristic& charact
       case 1:
         // for debugging/expanded functionality later
         break;
-      case 2:
+      case 2:    
         // for debugging/expanded functionality later
         break;
       case 3:
         // for debugging/expanded functionality later
-        break;
-   }
+        break;  
+   }   
 }
 void brightnessCharacteristicWritten(BLECentral& central, BLECharacteristic& characteristic)
 {
@@ -167,7 +166,7 @@ void brightnessCharacteristicWritten(BLECentral& central, BLECharacteristic& cha
   lampState.brightness = brightnessCharacteristic.value();
   ring.setBrightness(lampState.brightness);
   ring.show();
-
+  
 }
 void colorCharacteristicWritten(BLECentral& central, BLECharacteristic& characteristic)
 {
@@ -187,16 +186,16 @@ void lamp()
 {
   if (lampState.lampOn)
   {
-    /** do not set the lamp state variable lampState.brightness to 0
-     *  as the expectation of turning the lamp back on will be
+    /** do not set the lamp state variable lampState.brightness to 0 
+     *  as the expectation of turning the lamp back on will be 
      *  for the lamp to turn back on to the same brightness
-     *  as before turnng off.
+     *  as before turnng off. 
      *        Consider adding a dimming effect here.
      */
     ring.setBrightness(0);
     ring.show();
     lampState.lampOn = false;
-  }
+  } 
   else
   {
     ring.setBrightness( lampState.brightness );
@@ -208,3 +207,4 @@ void lamp()
     lampState.lampOn = true;
   }
 }
+
